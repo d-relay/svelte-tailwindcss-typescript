6c8f2f8d6b0e5995fc6796b4393ee9db48c69340
Andrij Tymchenko <xtymchenko@ex.ua>
2020-07-24 14:27:20 +0300

    added tailwindcss + typescript

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+const cssnano = require("cssnano")({ preset: "default" });
+const tailwindcss = require("tailwindcss");
+const autoprefixer = require('autoprefixer');
+
+const purgecss = require("@fullhuman/postcss-purgecss")({
+    content: ["./src/**/*.svelte", "./public/**/*.html"],
+    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+});
+
+const production = process.env.NODE_ENV === 'production';
+
+module.exports = {
+    plugins: [
+        tailwindcss, ...(production ? [purgecss, autoprefixer, cssnano] : []),
+    ]
+};
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    purge: ["./src/**/*.svelte", "./public/**/*.html"],
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [
+        // require('@tailwindcss/ui') comment out for adding tailwind css
+    ],
+}
